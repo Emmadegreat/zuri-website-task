8184d3a72a0f0e86b43b1c5fd4a79eb05b037964
Emmadegreat <mkpurunchiemma@gmail.com>
2022-08-20 13:39:17 -0700

    added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-const links = document.querySelectorAll('.link');
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        links.forEach(link => link.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
-
 /*======modal============*/
 const tri_1 = document.getElementById('tri_1');
 const mod_1 = document.getElementById('mod_1');
@@ -15,7 +7,7 @@ function Click() {
 }
 
 window.addEventListener("click", (e) => {
-    e.preventDefault();
+    //e.preventDefault();
     if (e.target === mod_1) {
         mod_1.style.visibility = 'hidden';
     }
@@ -25,16 +17,25 @@ const mod = document.querySelector(".mod");
 const toggle = document.querySelectorAll('.tri');
 toggle.forEach(tri => {
     tri.addEventListener('click', (e) => {
-        e.preventDefault();
+        //e.preventDefault();
         mod.classList.toggle("show-mod");
     })
 });
 
 window.addEventListener("click", (e) => {
-    e.preventDefault();
+    //e.preventDefault();
     if (e.target === mod) {
         mod.classList.toggle('show-mod');
     }
+});
+
+
+const links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        links.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
 });
 
 
