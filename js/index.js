3269cfd74ae717f4fb8cd7834a567daa8a35b70f
Emmadegreat <mkpurunchiemma@gmail.com>
2022-08-19 17:55:48 -0700

    added

--- js/index.js
+++ js/index.js
@@ -1,19 +1,39 @@
-const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
+const links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        links.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    })
+});
+/*const close = document.getElementById('close');
+const mod = document.getElementById('mod');
+const tri = document.getElementById('tri');
+
+tri.addEventListener('click',(e)=>{
+    e.preventDefault();
+    mod.classList.add('show-mod');
+});
+mod.addEventListener('click',(e)=>{
+    e.preventDefault();
+    mod.style.display = 'none';
+
+});*/
+const mod = document.querySelector(".mod");
+const tri = document.querySelector(".tri");
 const closeButton = document.querySelector(".close");
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
+function toggleMod() {
+    mod.classList.toggle("show-mod");
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
+    if (event.target === mod) {
         toggleModal();
     }
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+tri.addEventListener("click", toggleMod);
+closeButton.addEventListener("click", toggleMod);
+window.addEventListener("click", windowOnClick);*/
 
-//alert('welcomeeeee');
