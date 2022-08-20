ac0efd71d271deb80866db148958eb4da570cfac
Emmadegreat <mkpurunchiemma@gmail.com>
2022-08-19 19:13:47 -0700

    added

--- js/index.js
+++ js/index.js
@@ -1,27 +1,14 @@
 const links = document.querySelectorAll('.link');
 links.forEach(link => {
     link.addEventListener('click', (e) => {
-        e.preventDefault();
         links.forEach(link => link.classList.remove('active'));
         link.classList.add('active');
-    })
+    });
 });
-/*const close = document.getElementById('close');
-const mod = document.getElementById('mod');
-const tri = document.getElementById('tri');
 
-tri.addEventListener('click',(e)=>{
-    e.preventDefault();
-    mod.classList.add('show-mod');
-});
-mod.addEventListener('click',(e)=>{
-    e.preventDefault();
-    mod.style.display = 'none';
-
-});*/
 const mod = document.querySelector(".mod");
 const tri = document.querySelector(".tri");
-const closeButton = document.querySelector(".close");
+const close = document.querySelector(".close");
 
 function toggleMod() {
     mod.classList.toggle("show-mod");
@@ -29,11 +16,11 @@ function toggleMod() {
 
 function windowOnClick(event) {
     if (event.target === mod) {
-        toggleModal();
+        toggleMod();
     }
 }
 
 tri.addEventListener("click", toggleMod);
-closeButton.addEventListener("click", toggleMod);
-window.addEventListener("click", windowOnClick);*/
+window.addEventListener("click", windowOnClick);
+closeB.addEventListener("click", toggleMod);
 
